ac9fcbc91d0c49d3bafbe67963356826d5731a28
Sophie Feldman Bright <serbright1@sheffield.ac.uk>
2022-01-20 10:31:23 +0100

    SFB
    test of functionality

--- PHEDS_6.R
+++ PHEDS_6.R
@@ -2,14 +2,16 @@
 
 # Run the script below first, so that you can see all of the variables that are created.  Then follow steps 1-3 to make changes to the script.
  
-# 1. Add your full name as a new variable to the list below (to show what happens when you merge something new into the script)
+# 1. Add your full name as a new variable to the list below
 Sophie <- "Sophie Feldman Bright"
 
 # 2. Rewrite the variables below with your own favourite things (to show what happens when you change something in the script)
-Favourite_colour <- "yellow"
+Favourite_colour <- "purple"
 Favourite_animal <- "dog"
 
 # 3. Delete all of the script below
-Variable1 <- 10
-Variable2 <- 5+7
-Variable3 <- 8*10
+a <- 3+1
+b <- 5+5
+c <- 10*10
+
+# 4. Write whatever you want below as a comment (start the line with a #)
