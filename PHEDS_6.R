c6616df197d9543a93e367e93aafdadfecaf2609
Sophie Feldman Bright <serbright1@sheffield.ac.uk>
2022-01-20 10:16:42 +0100

    Create PHEDS-6.R

---
+++ PHEDS_6.R
@@ -0,0 +1,15 @@
+# Example script for PHEDS 6 cohort
+
+# Run the script below first, so that you can see all of the variables that are created.  Then follow steps 1-3 to make changes to the script.
+ 
+# 1. Add your full name as a new variable to the list below (to show what happens when you merge something new into the script)
+Sophie <- "Sophie Feldman Bright"
+
+# 2. Rewrite the variables below with your own favourite things (to show what happens when you change something in the script)
+Favourite_colour <- "yellow"
+Favourite_animal <- "dog"
+
+# 3. Delete all of the script below
+Variable1 <- 10
+Variable2 <- 5+7
+Variable3 <- 8*10
