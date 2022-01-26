eded45958973716c2cc779db1c2fc254c1e884db
pca00lcs <pca00lcs@sheffield.ac.uk>
2022-01-26 13:48:30 +0000

    Update PHEDS_6.R

--- PHEDS_6.R
+++ PHEDS_6.R
@@ -5,16 +5,16 @@
 # 1. Add your full name as a new variable to the list below
 Sophie <- "Sophie Feldman Bright"
 Ed <- "Ed Feldman Bright"
+Lucy <- "Lucy Burke"
 
 # 2. Rewrite the variables below with your own favourite things (to show what happens when you change something in the script)
-Favourite_colour <- "purple"
-Favourite_animal <- "dog"
+Favourite_colour <- "yellow"
+Favourite_animal <- "giraffe"
 
 # 3. Delete all of the script below
 
-a <- 3+1
-b <- 5+5
-c <- 10*10
+
 
 # 4. Write whatever you want below as a comment (start the line with a #)
 
+#has it worked?
